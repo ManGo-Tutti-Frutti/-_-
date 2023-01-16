201b32dc63f8120af648834ff4b8788b9f4ea369
kpa03 <kpa03091973@gmail.com>
2023-01-16 22:26:02 +1000

    Глава 5. Наш эксперимент почти завершён. Объект бегает, прыгает и даже кусается, это успех! Однако это ещё не конец, и мы продолжим наше исследование и доработку объекта...

--- Глава4.py
+++ Глава4.py
@@ -1,15 +1,12 @@
 import os
 import sys
 import pygame
-import random
+
+WIDTH, HEIGHT = 1200, 500
 
 
 class Tile(pygame.sprite.Sprite):
-    spisok = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    spisok = [[0 for j in range(WIDTH // 50)] for i in range(HEIGHT // 50)]
 
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(tiles_group, all_sprites)
@@ -27,24 +24,20 @@ class Tile(pygame.sprite.Sprite):
             tile_width * pos_x, tile_height * pos_y)
 
 
-def test(lst):
-    o = lst[-1:]+lst[:-1]
-    return o
-
-
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__(player_group, all_sprites)
         self.image = player_image
         self.rect = self.image.get_rect()
         self.cords = [pos_y, pos_x] #сначала у, потом x
-        self.rect.x = tile_width * pos_x + 15
-        self.rect.y = tile_height * pos_y + 5
+        self.nachalo = self.cords[:]
+        self.rect.x = tile_width * pos_x
+        self.rect.y = tile_height * pos_y
         self.y_up = False
 
     def update(self):
         for i in range(len(Tile.spisok)):
-            Tile.spisok[i] = test(Tile.spisok[i])
+            Tile.spisok[i] = Tile.spisok[i][-1:] + Tile.spisok[i][:-1]
 
     def jump(self):
         self.cords = [self.cords[0] - 1, self.cords[1]]
@@ -59,8 +52,16 @@ class Player(pygame.sprite.Sprite):
            self.rect = self.image.get_rect().move(tile_width * self.cords[1], tile_height * self.cords[0])
 
 
+def load_level(filename):
+    filename = "data/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
 def load_image(name, colorkey=None):
-    fullname = name
+    fullname = os.path.join('data', name)
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
@@ -71,11 +72,15 @@ def load_image(name, colorkey=None):
 def generate(map):
     for y in range(len(map)):
         for x in range(len(map[y])):
-            if map[y][x] == 'empty':
+            if map[y][x] == '.':
                 Tile('empty', x, y)
-            elif map[y][x] == 'wall':
+            elif map[y][x] == '&':
+                Tile('ice', x, y)
+            elif map[y][x] == '+':
+                Tile('snow', x, y)
+            elif map[y][x] == '#':
                 Tile('wall', x, y)
-            elif map[y][x] == 'player':
+            elif map[y][x] == '@':
                 Tile('empty', x, y)
                 new_player = Player(x, y)
     return new_player
@@ -96,7 +101,7 @@ def proverka(velikieschitovodi):
     if velikieschitovodi[2][1] != 0:
         velikieschitovodi[2][1] -= 1
     else:
-        velikieschitovodi[2][1] == 11
+        velikieschitovodi[2][1] = 11
     if velikieschitovodi[0] == player.cords or velikieschitovodi[1] == player.cords or velikieschitovodi[2] == player.cords:
         running = False
     return running
@@ -116,27 +121,23 @@ def start_screen():
         clock.tick(50)
 
 
-def end_screen():
-    fon = pygame.transform.scale(load_image('fon.png'), (1200, 500))
-    screen.blit(fon, (0, 0))
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
-        pygame.display.flip()
-        clock.tick(50)
+class Game_over(pygame.sprite.Sprite):
+    def __init__(self, image, *group):
+        super().__init__(*group)
+        self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect()
+        self.rect.x = -1200
+        self.rect.y = 0
+
+    def update(self):
+        x = self.rect.x
+        if x < 0:
+            self.rect.x += 15
 
 
 def terminate():
     pygame.quit()
     sys.exit()
-
-
-
-
 
 
 if __name__ == '__main__':
@@ -145,33 +146,27 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(size)
     FPS = 8
     clock = pygame.time.Clock()
-    tile_images = {'wall': load_image('box.png'), 'empty': load_image('grass.png')}
-    player_image = load_image('mario.png')
+    tile_images = {'wall': load_image('box.png'), 'empty': load_image('empty.png'), 'ice': load_image('ice.png'),
+                   'snow': load_image('snow.png')}
+    player_image = load_image('hero.png')
     tile_width = tile_height = 50
     all_sprites = pygame.sprite.Group()
     tiles_group = pygame.sprite.Group()
     player_group = pygame.sprite.Group()
-    map = [['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-           ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-           ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-           ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'wall', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'wall', 'empty'],
-           ['empty', 'empty', 'player', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'wall', 'wall', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'wall', 'empty'],
-           ['wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall'],
-           ['wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall'],
-           ['wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall'],
-           ['wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall'],
-           ['wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall']]
-    maps_size = (10, 12)
-    player = generate(map)
+    player = generate(load_level('map.txt'))
     start_screen()
     running = True
-    velikieschitovodi = [[3, 10], [4, 10], [4, 11]]
+    velikieschitovodi = [[3, 9], [4, 9], [4, 10]]
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
                 if pygame.key.get_pressed()[pygame.K_SPACE]:
+                    if player.cords == player.nachalo:
+                        player.y_up = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player.cords == player.nachalo:
                     player.y_up = True
         running = proverka(velikieschitovodi)
         player.update()
@@ -181,8 +176,16 @@ if __name__ == '__main__':
         player_group.draw(screen)
         clock.tick(FPS)
         pygame.display.flip()
+    clock = pygame.time.Clock()
+    all_sprites = pygame.sprite.Group()
+    game_over = Game_over('data\gameover.png', all_sprites)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        all_sprites.update()
+        all_sprites.draw(screen)
+        pygame.display.flip()
+        clock.tick(30)
     pygame.quit()
-    pygame.init()
-    size = width, height = 1200, 500
-    screen = pygame.display.set_mode(size)
-    end_screen()
